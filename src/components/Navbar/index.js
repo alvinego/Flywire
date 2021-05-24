c218981e04108c4f34849001bca93c6c42153d8c
Alvin Ego <alvin.ego@gmail.com>
2021-05-24 21:05:25 +0100

    Initaited Link Routers

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { NavbarContainer, Nav, NavLogo } from './NavbarElements';
+
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo>Flywire</NavLogo>
+                </NavbarContainer>
+            </Nav>
+        </>
+    );
+};
+
+export default Navbar
