cce257932c892c6c9ab5fa7a80cfdb4677b8bff8
Alvin Ego <alvin.ego@gmail.com>
2021-05-25 11:53:04 +0100

    Added Nav LInk About and React Scroll

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,13 +1,21 @@
 import React from 'react'
-import { NavbarContainer, Nav, NavLogo } from './NavbarElements';
-
+import { NavbarContainer, Nav, NavLogo, MobileIcon, NavItem, NavLinks, NavMenu } from './NavbarElements';
+import {FaBars} from 'react-icons/fa'
 
 const Navbar = () => {
     return (
         <>
             <Nav>
                 <NavbarContainer>
-                    <NavLogo>Flywire</NavLogo>
+                    <NavLogo to='/'>Flywire</NavLogo>
+                    <MobileIcon>
+                        <FaBars/>
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLinks to='about'>About</NavLinks>
+                        </NavItem>
+                    </NavMenu>
                 </NavbarContainer>
             </Nav>
         </>
