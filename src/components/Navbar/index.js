9c76abdbea4eb00fa6fba2e41f1f357608e4e003
Alvin Ego <alvin.ego@gmail.com>
2021-05-25 12:24:40 +0100

    Header Completed

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,14 @@
 import React from 'react'
-import { NavbarContainer, Nav, NavLogo, MobileIcon, NavItem, NavLinks, NavMenu } from './NavbarElements';
+import {
+    NavBtn,
+    NavBtnLink,
+    NavbarContainer,
+    Nav,
+    NavLogo,
+    MobileIcon,
+    NavItem,
+    NavLinks,
+    NavMenu} from './NavbarElements';
 import {FaBars} from 'react-icons/fa'
 
 const Navbar = () => {
@@ -15,7 +24,19 @@ const Navbar = () => {
                         <NavItem>
                             <NavLinks to='about'>About</NavLinks>
                         </NavItem>
+                        <NavItem>
+                            <NavLinks to='discover'>Discover</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='services'>Services</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='signup'>Sign up</NavLinks>
+                        </NavItem>
                     </NavMenu>
+                    <NavBtn>
+                        <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+                    </NavBtn>
                 </NavbarContainer>
             </Nav>
         </>
