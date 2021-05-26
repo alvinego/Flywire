403266b570b7735b3d1ed4fe58f55839111440b9
Alvin Ego <alvin.ego@gmail.com>
2021-05-26 14:55:15 +0100

    Added Hero Section and ButtonElements

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Button } from '../ButtonElement';
 import Video from '../../videos/video.mp4';
-import {HeroContainer, HeroBg, VideoBg} from './HeroElements';
+import {HeroContainer, 
+    HeroBg, 
+    VideoBg, 
+    HeroContent, 
+    HeroH1, 
+    HeroP, 
+    HeroBtnWrapper, 
+    ArrowForward, 
+    ArrowRight} from './HeroElements';
 
 const HeroSection = () => {
+    const [hover,setHover] = useState(false)
+
+    const onHover = () =>{
+        setHover(!hover)
+    }
+
     return (
         <HeroContainer>
             <HeroBg>
                 <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
             </HeroBg>
-            
+            <HeroContent>
+                <HeroH1>Get Infinite energy supply for your home and business need</HeroH1>
+                <HeroP>
+                    Sign up for a demo today and receive free energy supply for a week
+                </HeroP>
+                <HeroBtnWrapper>
+                    <Button to='signup' onMouseEnter={onHover} onMouseLeave={onHover} primary='true' dark='true'>
+                        Get Started {hover ? <ArrowForward/>: <ArrowRight />}
+                    </Button>
+                </HeroBtnWrapper>
+            </HeroContent>
         </HeroContainer>
     )
 }
